29cd0ac4fdfacc3d7cd5ab2706e4ff57f0e20717
parvez6084 <parvez.ka19@gmail.com>
2022-06-21 03:35:12 +0600

    remove welcome page

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -5,8 +5,6 @@ import '../views/pages/dashboard_page/dashboardPage.dart';
 import '../views/pages/dashboard_page/dashboardPageBinding.dart';
 import '../views/pages/home_page/homePage.dart';
 import '../views/pages/search_page/searchPage.dart';
-import '../views/pages/welcome_page/welcomePage.dart';
-import '../views/pages/welcome_page/welcomePageBinding.dart';
 import 'app_routes.dart';
 
 class AppPages {
@@ -14,11 +12,6 @@ class AppPages {
 
   static const INITIAL = Routes.dashBoardPage;
   static final routes = [
-    GetPage(
-      name: Routes.welcomePage,
-      page: () => WelcomePage(),
-      binding: WelcomePageBinding(),
-    ),
     GetPage(
         name: Routes.dashBoardPage,
         page: () => DashBoardPage(),
--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,6 +1,5 @@
 abstract class _Paths {
   static const dashBoardPagePath = '/';
-  static const welcomePagePath = '/welcomePage';
   static const homePagePath = '/homePage';
   static const scanPagePath = '/scanPage';
 }
@@ -9,7 +8,6 @@ abstract class Routes {
   Routes._();
 
   static const dashBoardPage = _Paths.dashBoardPagePath;
-  static const welcomePage = _Paths.welcomePagePath;
   static const homePage = _Paths.homePagePath;
   static const scanPage = _Paths.scanPagePath;
 
