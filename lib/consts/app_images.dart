90f0798c7ea640c85a7f50e4163330726ef40d86
parvez6084 <parvez.ka19@gmail.com>
2022-06-21 03:37:18 +0600

    remove useless item

--- lib/consts/app_images.dart
+++ lib/consts/app_images.dart
@@ -1,10 +1,3 @@
 class AppImages{
-  static const welcomeImage = 'assets/images/welcome_img.png';
   static const no_image = 'assets/images/no_image.jpg';
-  static const more = 'assets/images/more.png';
-  static const search = 'assets/images/search.png';
-  static const home = 'assets/images/home.png';
-  static const filter = 'assets/images/filter.png';
-  static const chat = 'assets/images/chat.png';
-
 }
